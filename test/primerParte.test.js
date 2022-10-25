77b6959ac91cccbb24a69fc74ac154a7c6eb79e8
jhuido <josefinahuidobro@uca.edu.ar>
2022-10-25 19:04:25 -0300

    TDD iteration

--- test/primerParte.test.js
+++ test/primerParte.test.js
@@ -3,3 +3,8 @@ test("Prueba de posicion apagada inicialmente",()=>{
     var posicion=new Posicion(0,0);
     expect(posicion.estadoActual()).toBe("Apagado");
 })
+
+test("Que no se puedan crear posiciones fuera del tablero",()=>{
+    expect(()=>{var posicion= new Posicion(0,100);}).toThrow(new Error("No se puede crear una posicion con eje en x mayor a 99"));
+    expect(()=>{var posicion= new Posicion(100,0);}).toThrow(new Error("No se puede crear una posicion con eje en y mayor a 99"));
+})
