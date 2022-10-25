5a1a652619df498448de4668655e32252f1c4081
jhuido <josefinahuidobro@uca.edu.ar>
2022-10-25 19:08:30 -0300

    TDD iteration

---
+++ src/Tablero.js
@@ -0,0 +1,14 @@
+function Tablero(){
+    this.tablero=[]
+    this.crearTablero()=function(){
+        var i=0;
+        var j=0;
+        for(i;i<100;i++){
+            for(j;j<100;j++){
+                this.tablero[i,j]=new Posicion(i,j);
+            }
+        }
+        return "Tablero creado";
+    }
+}
+module.exports=Tablero
