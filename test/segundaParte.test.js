fa55903c41537e242884ea4d7a133a4010779ebc
jhuido <josefinahuidobro@uca.edu.ar>
2022-10-25 19:36:42 -0300

    TDD iteration

--- test/segundaParte.test.js
+++ test/segundaParte.test.js
@@ -18,13 +18,13 @@ test("Tablero empiece apagado y funcion Encender",()=>{
 })
 
 test("Apagar",()=>{
-    var tablero=new Tablero();
+    var tablero=new Tablero2();
     tablero.crearTablero();
-    var posicionSuperior=new Posicion(1,0);
-    var posicionInferior= new Posicion(2,2);
+    var posicionSuperior=new Posicion2(1,0);
+    var posicionInferior= new Posicion2(2,2);
     tablero.encender(posicionSuperior,posicionInferior);
-    var posicionSup2=new Posicion(1,1);
-    var posocionInf2=new Posicion(2,2);
+    var posicionSup2=new Posicion2(1,1);
+    var posocionInf2=new Posicion2(2,2);
     tablero.apagar(posicionSup2,posocionInf2);
     expect(tablero.tablero[1,0].estadoActual()).toBe(1);
     expect(tablero.tablero[1,1].estadoActual()).toBe(0);
