c4d73eef9d82eeffb680159d0372d6eaddf9413f
jhuido <josefinahuidobro@uca.edu.ar>
2022-10-25 19:05:26 -0300

    TDD iteration

--- src/Posicion.js
+++ src/Posicion.js
@@ -1,4 +1,11 @@
 function Posicion(posiciony,posicionx){
+    if(posicionx>99){
+        throw new Error("No se puede crear una posicion con eje en x mayor a 99");
+    }
+    else if (posiciony>99){
+        throw new Error("No se puede crear una posicion con eje en y mayor a 99");
+    }
+    
     this.posicionx=posicionx;
     this.posiciony=posiciony;
     this.estado="Apagado"
