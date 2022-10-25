a4ab32245ecec845dbad6936f4849057169cf87b
jhuido <josefinahuidobro@uca.edu.ar>
2022-10-25 19:41:49 -0300

    TDD iteration

--- test/segundaParte.test.js
+++ test/segundaParte.test.js
@@ -33,3 +33,30 @@ test("Apagar",()=>{
     expect(tablero.tablero[2,1].estadoActual()).toBe(0);
     expect(tablero.tablero[2,2].estadoActual()).toBe(0);
 })
+
+test("Cambiar",()=>{
+
+
+    var tablero=new Tablero2();
+    tablero.crearTablero();
+    var posicionSuperior=new Posicion2(0,0);
+    var posicionInferior= new Posicion2(2,2);
+    tablero.encenderTablero(posicionSuperior,posicionInferior);
+
+    var posicionSup2=new Posicion2(0,0);
+    var posocionInf2=new Posicion2(2,1);
+
+    tablero.cambiar(posicionSup2,posocionInf2);
+
+    expect(tablero.tablero[0,0].estadoActual()).toBe(3);
+    expect(tablero.tablero[0,2].estadoActual()).toBe(1);
+    expect(tablero.tablero[1,0].estadoActual()).toBe(3);
+    expect(tablero.tablero[1,1].estadoActual()).toBe(3);
+    expect(tablero.tablero[1,2].estadoActual()).toBe(1);
+    expect(tablero.tablero[2,0].estadoActual()).toBe(3);
+    expect(tablero.tablero[2,1].estadoActual()).toBe(3);
+    expect(tablero.tablero[2,2].estadoActual()).toBe(1);
+
+
+
+})
